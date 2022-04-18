a2281036c31426f09eb3ea032a0fa2c3ff413d58
YousefDawoad <62189386+Yossef-Dawoad@users.noreply.github.com>
2022-04-18 12:17:41 +0200

    Update the main to plot live output from the Mic

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from record import Mic
 mic = Mic()
 
 
-# data = mic.output #* return Live output as real time data ->numpy array
+# data = mic.output #* return Live output as real time data as  -> numpy array
 
-# mic.record_toFile(duration=5,output_file="newfile.wav") #* record 5 second wav file
+mic.plot() ## live plot the ouput data from your microphone
 
-# mic.plot() #* live plot the ouput data from your microphone
+# mic.record_toFile(duration=5, output_file="newfile.wav") ## record 5 second wav file
 
 #? New Functionality
 # mic.analysis.getfft() #* apply fourier transform to the output of the data -> numpy array
